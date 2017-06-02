4e53544e4393f02e7670925c914a61e0022185fe
Joe Huss <detain@interserver.net>
2017-06-02 07:07:32 -0400

    updates working on the post payment processing handling of addons and the vps module

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,26 +10,29 @@ class Plugin {
 	}
 
 	public static function Load(GenericEvent $event) {
-		$service = $event->getSubject();
+		$service_order = $event->getSubject();
 		function_requirements('class.Addon');
 		$addon = new \Addon();
 		$addon->set_module('vps')->set_text('CPanel')->set_cost(VPS_CPANEL_COST)
-			->set_require_ip(true)->set_enable(function() {
+			->set_require_ip(true)->set_enable(function() use ($service_order) {
+				$service_info = $service_order->get_service_info();
+				$settings = get_module_settings($service_order->get_module());
 				require_once 'include/licenses/license.functions.inc.php';
 				function_requirements('get_license_data');
 				function_requirements('activate_cpanel');
-				$service_extra = get_license_data($this->service_info[$this->settings['PREFIX'].'_ip']);
+				$service_extra = get_license_data($service_info[$settings['PREFIX'].'_ip']);
 				// check if activated,if not then activate cpanel license
-				if (($service_extra['valid'] != 1) && ($ip != '')) {
+				if (($service_extra['valid'] != 1) && ($service_info[$settings['PREFIX'].'_ip'] != '')) {
 					// 188 = openvz , 1814 = kvm
-					if (in_array($service_type, array(SERVICE_TYPES_KVM_LINUX, SERVICE_TYPES_CLOUD_KVM_LINUX), true))
-						activate_cpanel($ip, 1814);
+					if (in_array($service_info[$settings['PREFIX'].'_type'], array(SERVICE_TYPES_KVM_LINUX, SERVICE_TYPES_CLOUD_KVM_LINUX), true))
+						activate_cpanel($service_info[$settings['PREFIX'].'_ip'], 1814);
 					else
-						activate_cpanel($ip, 188);
-					$GLOBALS['tf']->history->add($settings['TABLE'], 'add_'.$this->get_short(), $this->service_info[$this->settings['PREFIX'].'_id'], $this->service_info[$this->settings['PREFIX'].'_ip'], $this->service_info[$this->settings['PREFIX'].'_custid']);
+						activate_cpanel($service_info[$settings['PREFIX'].'_ip'], 188);
+					$GLOBALS['tf']->history->add($settings['TABLE'], 'add_cpanel', $service_info[$settings['PREFIX'].'_id'], $service_info[$settings['PREFIX'].'_ip'], $service_info[$settings['PREFIX'].'_custid']);
 				}
-			})->set_disable(function() {
+			})->set_disable(function($service) {
 			})->register();
+		$service_order->add_addon($addon);
 	}
 
 }
