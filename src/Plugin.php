82144a2ba2b926c48355f67b9197bad499d429f5
Joe Huss <detain@interserver.net>
2017-06-15 06:21:57 -0400

    updates switchign vps addon to using udpated way of registering and adding in disable event

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,26 +13,47 @@ class Plugin {
 		$service_order = $event->getSubject();
 		function_requirements('class.Addon');
 		$addon = new \Addon();
-		$addon->set_module('vps')->set_text('CPanel')->set_cost(VPS_CPANEL_COST)
-			->set_require_ip(true)->set_enable(function() use ($service_order) {
-				$service_info = $service_order->get_service_info();
-				$settings = get_module_settings($service_order->get_module());
-				require_once 'include/licenses/license.functions.inc.php';
-				function_requirements('get_cpanel_license_data_by_ip');
-				function_requirements('activate_cpanel');
-				$service_extra = get_cpanel_license_data_by_ip($service_info[$settings['PREFIX'].'_ip']);
-				// check if activated,if not then activate cpanel license
-				if (($service_extra['valid'] != 1) && ($service_info[$settings['PREFIX'].'_ip'] != '')) {
-					// 188 = openvz , 1814 = kvm
-					if (in_array($service_info[$settings['PREFIX'].'_type'], array(SERVICE_TYPES_KVM_LINUX, SERVICE_TYPES_CLOUD_KVM_LINUX), true))
-						activate_cpanel($service_info[$settings['PREFIX'].'_ip'], 1814);
-					else
-						activate_cpanel($service_info[$settings['PREFIX'].'_ip'], 188);
-					$GLOBALS['tf']->history->add($settings['TABLE'], 'add_cpanel', $service_info[$settings['PREFIX'].'_id'], $service_info[$settings['PREFIX'].'_ip'], $service_info[$settings['PREFIX'].'_custid']);
-				}
-			})->set_disable(function($service) {
-			})->register();
+		$addon->set_module('vps')
+			->set_text('CPanel')
+			->set_cost(VPS_CPANEL_COST)
+			->set_require_ip(true)
+			->set_enable(['Detain\MyAdminVpsCpanel\Plugins', 'Enable'])
+			->set_disable(['Detain\MyAdminVpsCpanel\Plugins', 'Disable'])
+			->register();
 		$service_order->add_addon($addon);
+	}
+
+	public static function Enable($service_order) {
+		$service_info = $service_order->get_service_info();
+		$settings = get_module_settings($service_order->get_module());
+		require_once 'include/licenses/license.functions.inc.php';
+		function_requirements('get_cpanel_license_data_by_ip');
+		$service_extra = get_cpanel_license_data_by_ip($service_info[$settings['PREFIX'].'_ip']);
+		// check if activated,if not then activate cpanel license
+		if (($service_extra['valid'] != 1) && ($service_info[$settings['PREFIX'].'_ip'] != '')) {
+			function_requirements('activate_cpanel');
+			// 188 = openvz , 1814 = kvm
+			if (in_array($service_info[$settings['PREFIX'].'_type'], array(SERVICE_TYPES_KVM_LINUX, SERVICE_TYPES_CLOUD_KVM_LINUX), true))
+				activate_cpanel($service_info[$settings['PREFIX'].'_ip'], 1814);
+			else
+				activate_cpanel($service_info[$settings['PREFIX'].'_ip'], 188);
+			$GLOBALS['tf']->history->add($settings['TABLE'], 'add_cpanel', $service_info[$settings['PREFIX'].'_id'], $service_info[$settings['PREFIX'].'_ip'], $service_info[$settings['PREFIX'].'_custid']);
+		}
+	}
+
+	public static function Disable($service_order) {
+		$service_info = $service_order->get_service_info();
+		$settings = get_module_settings($service_order->get_module());
+		require_once 'include/licenses/license.functions.inc.php';
+		function_requirements('get_cpanel_license_data_by_ip');
+		$service_extra = get_cpanel_license_data_by_ip($service_info[$settings['PREFIX'].'_ip']);
+		// check if activated,if so then deactivate cpanel license
+		if (($service_extra['valid'] != 1) && ($service_info[$settings['PREFIX'].'_ip'] != '')) {
+			function_requirements('deactivate_cpanel');
+			deactivate_cpanel($service_info[$settings['PREFIX'].'_ip']);
+			$GLOBALS['tf']->history->add($settings['TABLE'], 'del_cpanel', $service_info[$settings['PREFIX'].'_id'], $service_info[$settings['PREFIX'].'_ip'], $service_info[$settings['PREFIX'].'_custid']);
+		}
+
 	}
 
 	public static function Settings(GenericEvent $event) {
