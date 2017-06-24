16f114cbdec5c5fef22754878ca62473058fca3a
Joe Huss <detain@interserver.net>
2017-06-24 14:53:12 -0400

    a bunch of updates improving the code making use of self:: and other imprvoements

--- src/Plugin.php
+++ src/Plugin.php
@@ -18,8 +18,8 @@ class Plugin {
 
 	public static function getHooks() {
 		return [
-			'vps.load_addons' => [__CLASS__, 'getAddon'],
-			'vps.settings' => [__CLASS__, 'getSettings'],
+			self::$module.'.load_addons' => [__CLASS__, 'getAddon'],
+			self::$module.'.settings' => [__CLASS__, 'getSettings'],
 		];
 	}
 
@@ -27,7 +27,7 @@ class Plugin {
 		$serviceOrder = $event->getSubject();
 		function_requirements('class.Addon');
 		$addon = new \Addon();
-		$addon->setModule('vps')
+		$addon->setModule(self::$module)
 			->set_text('CPanel')
 			->set_cost(VPS_CPANEL_COST)
 			->set_require_ip(TRUE)
@@ -39,7 +39,7 @@ class Plugin {
 
 	public static function doEnable(\Service_Order $serviceOrder, $repeatInvoiceId, $regexMatch = false) {
 		$serviceInfo = $serviceOrder->getServiceInfo();
-		$settings = get_module_settings($serviceOrder->getModule());
+		$settings = get_module_settings(self::$module);
 		require_once __DIR__.'/../../../../include/licenses/license.functions.inc.php';
 		function_requirements('get_cpanel_license_data_by_ip');
 		$serviceExtra = get_cpanel_license_data_by_ip($serviceInfo[$settings['PREFIX'].'_ip']);
@@ -57,7 +57,7 @@ class Plugin {
 
 	public static function doDisable(\Service_Order $serviceOrder) {
 		$serviceInfo = $serviceOrder->getServiceInfo();
-		$settings = get_module_settings($serviceOrder->getModule());
+		$settings = get_module_settings(self::$module);
 		require_once __DIR__.'/../../../../include/licenses/license.functions.inc.php';
 		function_requirements('get_cpanel_license_data_by_ip');
 		$serviceExtra = get_cpanel_license_data_by_ip($serviceInfo[$settings['PREFIX'].'_ip']);
