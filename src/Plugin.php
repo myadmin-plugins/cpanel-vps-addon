ad068901f7d53954836d506ecbd9b55d6edc81b1
Joe Huss <detain@interserver.net>
2019-11-16 10:48:18 -0500

    a bunch of updates switching out old admin_mail code with the new Mail class adminMail call and optmizing things here and there as i go'

--- src/Plugin.php
+++ src/Plugin.php
@@ -114,11 +114,7 @@ class Plugin
 			add_output(self::$name.' Canceled');
 			$email = $settings['TBLNAME'].' ID: '.$serviceInfo[$settings['PREFIX'].'_id'].'<br>'.$settings['TBLNAME'].' Hostname: '.$serviceInfo[$settings['PREFIX'].'_hostname'].'<br>Repeat Invoice: '.$repeatInvoiceId.'<br>Description: '.self::$name.'<br>';
 			$subject = $settings['TBLNAME'].' '.$serviceInfo[$settings['PREFIX'].'_id'].' Canceled '.self::$name;
-			$headers = '';
-			$headers .= 'MIME-Version: 1.0'.PHP_EOL;
-			$headers .= 'Content-type: text/html; charset=UTF-8'.PHP_EOL;
-			$headers .= 'From: '.$settings['TITLE'].' <'.$settings['EMAIL_FROM'].'>'.PHP_EOL;
-			admin_mail($subject, $email, $headers, false, 'admin/vps_cpanel_canceled.tpl');
+			(new MyAdmin\Mail())->adminMail($subject, $email, false, 'admin/vps_cpanel_canceled.tpl');
 		}
 	}
 
